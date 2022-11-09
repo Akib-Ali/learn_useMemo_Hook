4e27f3eeb1e201fd3804dce6a9ba2aaa70e6836a
Akib Ali <akib7599@gmail.com>
2022-11-09 18:03:27 +0530

    learn use Memo

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { WithOutUseMemo } from './withOutUseMemo/withoutusememo'
+
+function App() {
+
+  
+  
+
+  return (
+    <div className="App">
+    <WithOutUseMemo/>
+    </div>
+  )
+}
+
+export default App
